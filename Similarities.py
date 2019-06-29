2ef572e125a5226aacc8132ad661914074f92aca
Cloves <cacl@cin.ufpe.br>
2019-06-29 14:04:03 -0300

    create receive directory arg

--- Similarities.py
+++ Similarities.py
@@ -5,6 +5,10 @@ import re, math
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk import tokenize
+import argparse
+import os
+from pathlib import Path
+
 
 # Global variables
 WORD = re.compile(r'\w+')
@@ -80,22 +84,31 @@ def get_pontuation(text):
 
 
 if __name__ == '__main__':
-    # Documents or Texts
-    test1 = "texto"
-    test2 = "texto"
 
+    lines = []
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', type=str,nargs=2, metavar='dir', help='directories of documents .txt. OBS.: First .txt might be the text recovered by API')
+    args = parser.parse_args()
+    filename = args.d
+    for line in filename:
+        lines.append(Path(line).read_text().splitlines())
+
+    # Documents or Texts
+    test1 = lines[0]
+    test2 = lines[1]
+    
     # Get punctuation
-    numberOfPunctuation = get_pontuation(test1)
+    numberOfPunctuation = get_pontuation(test1[0])
 
     # Stopwords removal
     # test1 = text_normalized(test1)
     # test2 = text_normalized(test2)
 
     # Similatities results
-    print("WER:", "%.2f" % get_wer(test1, test2))
-    print("Jaccard:", "%.2f" % get_jaccard(test1, test2))
-    print("Levenshtein:", get_levenshtein(test1, test2))
-    print('Cosine:', "%.2f" % get_cosine(test1, test2))
+    print("WER:", "%.2f" % get_wer(test1[0], test2[0]))
+    print("Jaccard:", "%.2f" % get_jaccard(test1[0], test2[0]))
+    print("Levenshtein:", get_levenshtein(test1[0], test2[0]))
+    print('Cosine:', "%.2f" % get_cosine(test1[0], test2[0]))
 
     # Punctuation results
     print('Quantidade de Pontos:', numberOfPunctuation[0])
